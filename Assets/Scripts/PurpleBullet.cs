fd771f4d10b8ee6200777a859ea2bf30f3912871
unknown <Stacia@DESKTOP-82HCPJ2>
2020-06-09 22:22:58 +0700

    new merge

---
+++ Assets/Scripts/PurpleBullet.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PurpleBullet : MonoBehaviour
+{
+	private float speed  = 4;
+	private Vector2 dir;
+    // Start is called before the first frame update
+    void Start()
+    {
+        dir = GameObject.Find("Dir").transform.position;
+        transform.position = GameObject.Find("FirePoint").transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, dir, speed *  Time.deltaTime);
+    }
+}
